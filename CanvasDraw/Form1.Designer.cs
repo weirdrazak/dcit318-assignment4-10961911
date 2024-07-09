361c049a47efed3a312092df8b2410f9b9d637dc
Razak Appiah <weirdrazak>
2024-07-09 12:58:32 +0000

    Added a drawing application

---
+++ CanvasDraw/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace CanvasDraw
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panelCanvas;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RadioButton radioButtonSketch;
+        private System.Windows.Forms.RadioButton radioButtonLine;
+        private System.Windows.Forms.RadioButton radioButtonRectangle;
+        private System.Windows.Forms.RadioButton radioButtonEllipse;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelCanvas = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.radioButtonSketch = new System.Windows.Forms.RadioButton();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonEllipse = new System.Windows.Forms.RadioButton();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // panelCanvas
+            // 
+            this.panelCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCanvas.Location = new System.Drawing.Point(0, 0);
+            this.panelCanvas.Name = "panelCanvas";
+            this.panelCanvas.Size = new System.Drawing.Size(800, 400);
+            this.panelCanvas.TabIndex = 0;
+            this.panelCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.panelCanvas_Paint);
+            this.panelCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseDown);
+            this.panelCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseMove);
+            this.panelCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseUp);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(700, 10);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 30);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // radioButtonSketch
+            // 
+            this.radioButtonSketch.AutoSize = true;
+            this.radioButtonSketch.Location = new System.Drawing.Point(10, 10);
+            this.radioButtonSketch.Name = "radioButtonSketch";
+            this.radioButtonSketch.Size = new System.Drawing.Size(73, 21);
+            this.radioButtonSketch.TabIndex = 2;
+            this.radioButtonSketch.TabStop = true;
+            this.radioButtonSketch.Text = "Sketch";
+            this.radioButtonSketch.UseVisualStyleBackColor = true;
+            this.radioButtonSketch.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButtonLine
+            // 
+            this.radioButtonLine.AutoSize = true;
+            this.radioButtonLine.Location = new System.Drawing.Point(100, 10);
+            this.radioButtonLine.Name = "radioButtonLine";
+            this.radioButtonLine.Size = new System.Drawing.Size(55, 21);
+            this.radioButtonLine.TabIndex = 3;
+            this.radioButtonLine.TabStop = true;
+            this.radioButtonLine.Text = "Line";
+            this.radioButtonLine.UseVisualStyleBackColor = true;
+            this.radioButtonLine.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButtonRectangle
+            // 
+            this.radioButtonRectangle.AutoSize = true;
+            this.radioButtonRectangle.Location = new System.Drawing.Point(190, 10);
+            this.radioButtonRectangle.Name = "radioButtonRectangle";
+            this.radioButtonRectangle.Size = new System.Drawing.Size(95, 21);
+            this.radioButtonRectangle.TabIndex = 4;
+            this.radioButtonRectangle.TabStop = true;
+            this.radioButtonRectangle.Text = "Rectangle";
+            this.radioButtonRectangle.UseVisualStyleBackColor = true;
+            this.radioButtonRectangle.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButtonEllipse
+            // 
+            this.radioButtonEllipse.AutoSize = true;
+            this.radioButtonEllipse.Location = new System.Drawing.Point(300, 10);
+            this.radioButtonEllipse.Name = "radioButtonEllipse";
+            this.radioButtonEllipse.Size = new System.Drawing.Size(71, 21);
+            this.radioButtonEllipse.TabIndex = 5;
+            this.radioButtonEllipse.TabStop = true;
+            this.radioButtonEllipse.Text = "Ellipse";
+            this.radioButtonEllipse.UseVisualStyleBackColor = true;
+            this.radioButtonEllipse.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Controls.Add(this.radioButtonSketch);
+            this.flowLayoutPanel.Controls.Add(this.radioButtonLine);
+            this.flowLayoutPanel.Controls.Add(this.radioButtonRectangle);
+            this.flowLayoutPanel.Controls.Add(this.radioButtonEllipse);
+            this.flowLayoutPanel.Controls.Add(this.btnClear);
+            this.flowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(0, 400);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(800, 50);
+            this.flowLayoutPanel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelCanvas);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Name = "Form1";
+            this.Text = "CanvasDraw";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ CanvasDraw/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CanvasDraw
+{
+    public partial class Form1 : Form
+    {
+        private bool _isDrawing;
+        private Point _startPoint;
+        private Point _endPoint;
+        private Graphics _graphics;
+        private Bitmap _bitmap;
+        private string _drawMode;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _isDrawing = false;
+            _bitmap = new Bitmap(panelCanvas.Width, panelCanvas.Height);
+            _graphics = Graphics.FromImage(_bitmap);
+            panelCanvas.BackgroundImage = _bitmap;
+            _drawMode = "Sketch"; // Default draw mode
+        }
+
+        private void panelCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _isDrawing = true;
+                _startPoint = e.Location;
+                if (_drawMode == "Sketch")
+                {
+                    _endPoint = e.Location;
+                }
+            }
+        }
+
+        private void panelCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDrawing)
+            {
+                _endPoint = e.Location;
+                if (_drawMode == "Sketch")
+                {
+                    _graphics.DrawLine(Pens.Black, _startPoint, _endPoint);
+                    _startPoint = _endPoint;
+                    panelCanvas.Invalidate();
+                }
+                else
+                {
+                    panelCanvas.Invalidate();
+                }
+            }
+        }
+
+        private void panelCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _isDrawing = false;
+                if (_drawMode != "Sketch")
+                {
+                    DrawShape(_graphics, _startPoint, _endPoint);
+                    panelCanvas.Invalidate(); // Refresh the panel to show the drawing
+                }
+            }
+        }
+
+        private void panelCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (_isDrawing && _drawMode != "Sketch")
+            {
+                DrawShape(e.Graphics, _startPoint, _endPoint);
+            }
+        }
+
+        private void DrawShape(Graphics g, Point start, Point end)
+        {
+            switch (_drawMode)
+            {
+                case "Line":
+                    g.DrawLine(Pens.Black, start, end);
+                    break;
+                case "Rectangle":
+                    g.DrawRectangle(Pens.Black, GetRectangle(start, end));
+                    break;
+                case "Ellipse":
+                    g.DrawEllipse(Pens.Black, GetRectangle(start, end));
+                    break;
+            }
+        }
+
+        private Rectangle GetRectangle(Point start, Point end)
+        {
+            return new Rectangle(
+                Math.Min(start.X, end.X),
+                Math.Min(start.Y, end.Y),
+                Math.Abs(start.X - end.X),
+                Math.Abs(start.Y - end.Y));
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            _graphics.Clear(Color.White);
+            panelCanvas.Invalidate(); // Refresh the panel to clear the drawing
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonSketch.Checked)
+                _drawMode = "Sketch";
+            else if (radioButtonLine.Checked)
+                _drawMode = "Line";
+            else if (radioButtonRectangle.Checked)
+                _drawMode = "Rectangle";
+            else if (radioButtonEllipse.Checked)
+                _drawMode = "Ellipse";
+        }
+    }
+}
